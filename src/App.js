7871404950a4308e40474160d836dcac2d31a514
raulojeda22 <raulojeda10g@gmail.com>
2020-06-03 20:06:48 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  stopAnimation(e) {
+    e.target.classList.add("stopAnimation");
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 onClick={(e) => this.stopAnimation(e)}>Raül</h1>
+        <nav>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/raulojeda22">Github</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/raul_ojeda22/">Instagram</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/raulojeda22/">LinkedIn</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://donrapido.es/">Don Rápido</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.silverand.es/">Silver and...</a>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
